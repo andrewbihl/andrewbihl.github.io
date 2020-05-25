ba4ddd109ede55f0f7007351b9626915a3388596
andrewbihl <andrewbihlva@gmail.com>
2020-05-25 14:51:09 -0700

    Remove the Montserrat font because I dont like it

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,7 +61,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
+        name: `g Starter Blog`,
         short_name: `GatsbyJS`,
         start_url: `/`,
         background_color: `#ffffff`,
